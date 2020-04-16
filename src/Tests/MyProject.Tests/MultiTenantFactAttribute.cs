6d7e548e8769891344664e1f7e6e711704126044
dinghongxin <771801383@qq.com>
2020-04-16 14:22:30 +0800

    Init

---
+++ src/Tests/MyProject.Tests/MultiTenantFactAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace MyProject.Tests
+{
+    public sealed class MultiTenantFactAttribute : FactAttribute
+    {
+        public MultiTenantFactAttribute()
+        {
+        }
+    }
+}
